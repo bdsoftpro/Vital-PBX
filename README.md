b44b7687b0c2d19c5b92f98edbd3dc5d816ef8b9
Md Delwar Hossain <delwar0cse@gmail.com>
2020-01-26 00:01:42 +0600

    Vital PBX CentOS 7.7

---
+++ README.md
@@ -0,0 +1,40 @@
+# VitalPBX VPS
+Script and resources to install VitalPBX on VPS Machines with **Centos 7 (x86_64)**
+
+- **[How to Used](#how-to-used)**
+- **[Troubleshooting](#troubleshooting)**
+- **[Important Note](#important-note)**
+
+## How to Used
+1. If you don't have installed __wget__ command, install it in the following way:
+<pre>
+yum install wget -y
+</pre>
+2. Download the script:
+<pre>
+wget https://raw.githubusercontent.com/VitalPBX/VPS/master/vps.sh
+</pre>
+3. Set correct permissions to script:
+<pre>
+chmod +x vps.sh
+</pre>
+4. Excute the script to install VitalPBX on VPS:
+<pre>
+./vps.sh
+</pre>
+
+## Troubleshooting
+1. When apply changes appears the following message: __sudo: unable to open audit system__,
+
+ ![SELINUX ERROR](https://github.com/VitalPBX/VPS/blob/master/resources/selinux.jpg?raw=true)
+
+This message appears when the SELINUX is enabled, for disabled execute the following command and then reboot your system:
+<pre>
+sed -i 's/^SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
+reboot
+</pre>
+
+## Important Note
+VitalPBX is not working with OpenVZ based VPS, please, use KVM based VPS.
+
+Due OpenVZ share the kernel and system files with the other users on the node and the host it's self, you are not able to modify the Kernel in any possible way, so, some applications like fail2ban does will not work as expected.
---
+++ vps.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+set -e
+
+#Disable Selinux Temporarily
+SELINUX_STATUS=$(getenforce)
+if [ "$SELINUX_STATUS" != "Disabled" ]; then
+    echo "Disabling SELINUX Temporarily"
+    setenforce 0
+else
+  echo "SELINUX it is already disabled"
+fi
+
+#Disable SeLinux Permanently
+sefile="/etc/selinux/config"
+if [ -e $sefile ]
+then
+  sed -i 's/^SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
+fi
+
+#Clean Yum Cache
+yum clean all
+rm -rf /var/cache/yum
+
+#Download the beta repo of VitalPBX
+rm -rf /etc/yum.repos.d/vitalpbx.repo
+wget -P /etc/yum.repos.d/ https://raw.githubusercontent.com/VitalPBX/VPS/master/resources/vitalpbx.repo
+
+#Install SSH Welcome Banner
+rm -rf /etc/profile.d/vitalwelcome.sh
+wget -P /etc/profile.d/ https://raw.githubusercontent.com/VitalPBX/VPS/master/resources/vitalwelcome.sh
+chmod 644 /etc/profile.d/vitalwelcome.sh
+
+#Intall other required dependencies
+yum -y install epel-release php
+
+# Update the system & Clean Cache Again
+yum clean all
+rm -rf /var/cache/yum
+yum -y update
+
+#Install MariaDB (MySQL)
+yum install mariadb-server -y
+systemctl enable mariadb
+rm -rf /etc/my.cnf.d/ombutel.cnf
+wget -P /etc/my.cnf.d/ https://raw.githubusercontent.com/VitalPBX/VPS/master/resources/ombutel.cnf
+systemctl start mariadb
+
+# Install VitalPBX pre-requisites
+wget https://raw.githubusercontent.com/VitalPBX/VPS/master/resources/pack_list
+yum -y install $(cat pack_list)
+
+# Install VitalPBX
+mkdir -p /etc/ombutel
+mkdir -p /etc/asterisk/ombutel
+yum -y install vitalpbx vitalpbx-asterisk-configs vitalpbx-fail2ban-config vitalpbx-sounds vitalpbx-themes dahdi-linux dahdi-tools dahdi-tools-doc kmod-dahdi-linux fxload
+
+# Speed up the localhost name resolving
+sed -i 's/^hosts.*$/hosts:      myhostname files dns/' /etc/nsswitch.conf
+
+cat << EOF >> /etc/sysctl.d/10-ombutel.conf
+# Reboot machine automatically after 20 seconds if it kernel panics
+kernel.panic = 20
+EOF
+
+# Set permissions
+chown -R apache:root /etc/asterisk/ombutel
+
+# Restart httpd
+systemctl restart httpd
+
+#Start vpbx-setup.service
+systemctl start vpbx-setup.service
+
+# Enable the http access:
+firewall-cmd --add-service=http
+firewall-cmd --reload
+
+# Reboot System to Make Selinux Change Permanently
+echo "Rebooting System"
+reboot
